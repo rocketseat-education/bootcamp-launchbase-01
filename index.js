cf32080aa34ee76f9d0a5bce61c9813ee93125b2
Mayk Brito <mayk@Mayks-MacBook-Air.local>
2019-10-15 16:09:31 -0300

    05
    Functions

--- index.js
+++ index.js
@@ -17,12 +17,35 @@ const alunos = [
   }
 ]
 
-const media = (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3;
+
+const alunos2 = [
+  {
+    nome: "Cleiton",
+    nota: 10
+  },
+  {
+    nome: "Robson",
+    nota: 10
+  },
+  {
+    nome: "Jonh",
+    nota: 2
+  }
+]
+
+function calculateMedia(alunos) {
+  const media = (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3;
+  
+  if (media > 5) {
+    console.log(`A média foi de ${media} e está acima!`)
+  }
+  else {
+    console.log(`A média foi de ${media} e está abaixo.`)
+  }
+}
+
+calculateMedia(alunos)
+calculateMedia(alunos2)
 
 
-if (media > 5) {
-  console.log(`A média foi de ${media} e está acima!`)
-}
-else {
-  console.log(`A média foi de ${media} e está abaixo.`)
-}
+
