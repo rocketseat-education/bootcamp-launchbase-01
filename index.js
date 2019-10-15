926cb35e28f5c51eedba5aea7b26609abfab451b
Mayk Brito <mayk@Mayks-MacBook-Air.local>
2019-10-15 17:35:27 -0300

    01

---
+++ index.js
@@ -0,0 +1,16 @@
+// Criar um programa que calcula a média 
+// das notas entre os alunos e envia 
+// mensagem do cálculo da média. 
+
+const nomeAluno01 = "Mayk"
+const notaAluno01 = 9.8
+
+const nomeAluno02 = "Diego"
+const notaAluno02 = 10
+
+const nomeAluno03 = "Fulano"
+const notaAluno03 = 2
+
+const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3;
+
+console.log(media)
