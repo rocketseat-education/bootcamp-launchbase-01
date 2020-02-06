289152df56dc28ddac1bd10e1e30f5cee61e427f
Mayk Brito <maykbrito@gmail.com>
2020-02-06 10:00:10 -0400

    chore: add file

---
+++ operadores.js
@@ -0,0 +1,79 @@
+/* ===================================================   
+    OPERADORES DE COMPARAÇÃO
+    
+    >       Maior
+    <       Menor
+    >=      Maior igual a
+    <=      Menor igual a
+    ==      Igual a
+    ===     Igual e do mesmo tipo
+    !=      Diferente de 
+    !==     Diferente, inclusive do tipo
+    
+======================================================*/
+
+
+// // DESAFIO 1
+// const idade = 17
+// // verificar se a pessoa é maior igual a 18 anos
+// // se sim, deixar entrar, se não, bloquear a entrada
+// if(idade >= 18) {
+//     console.log('Deixar entrar')
+// } else {
+//     console.log('Bloquear a entrada')
+// }
+
+// // se a pessoa tiver 17 anos
+// // avisar para voltar quando fizer 18 anos
+// if (idade === 17) {
+//     console.log('Volte quando tiver 18')
+// }
+
+
+
+/* ==========================================   
+    OPERADORES DE LÓGICOS
+    
+    &&  "E" As duas condições devem ser verdadeiras
+        para que a condição final seja verdadeira.
+    ||  "OU" Uma das condições deve ser verdadeira
+        para que a condição final seja verdadeira.
+    !   "NÃO" Nega uma condição
+    
+=============================================*/
+
+
+
+
+// // DESAFIO 1
+// const idade = 18
+// // verificar se a pessoa é maior igual a 18 anos
+// // se sim, deixar entrar, se não, bloquear a entrada
+// // se a pessoa tiver 17 anos
+// // avisar para voltar quando fizer 18 anos
+
+// if(!(idade >= 18) || idade === 17) {
+//     console.log('Bloquear a entrada')
+// } else {
+//     console.log('Deixar entrar')
+// }
+
+
+
+
+/* ==========================================   
+    OPERADORES DE ARITMÉTICOS
+    
+    *   Multiplicação
+    /   Divisão
+    %   Resto da divisão
+    +   Adição
+    -   Subtração
+
+=============================================*/
+
+console.log( 2 * 2) // 4
+console.log( 2 / 2) // 1
+console.log( 2 % 1.5) // 0.5
+console.log( 2 + 2) // 4
+console.log( 2 - 2) // 0
